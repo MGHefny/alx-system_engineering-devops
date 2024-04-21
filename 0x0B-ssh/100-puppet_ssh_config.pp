9bffec1418ff2bfaa065c51dd340ef45c66dee10
MGHefny <mgenedhefny@gmail.com>
2024-04-22 00:00:41 +0200

    adv

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#ssh config
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  ensure => 'present',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  ensure => 'present',
+  line   => '    IdentityFile ~/.ssh/school',
+}

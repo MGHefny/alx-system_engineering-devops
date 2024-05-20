fac7829a770a10243044a393498365a9e138facc
MGHefny <mgenedhefny@gmail.com>
2024-05-21 02:08:09 +0300

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,21 +7,23 @@ import re
 
 api_url = "https://jsonplaceholder.typicode.com"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) > 1:
-        if re.match(r"^\d+$", sys.argv[1]):
+        if re.match(r'^\d+$', sys.argv[1]):
             id = int(sys.argv[1])
-            user_respo = requests.get(f"{api_url}/users/{id}").json()
-            task_respo = requests.get(f"{api_url}/todos").json()
-            name_emplo = user_respo.get("name")
-            to_do = [task for task in task_respo if task.get("userId") == id]
-            comp_task = [task for task in to_do if task.get("completed")]
-
+            user_respo = requests.get(f'{api_url}/users/{id}').json()
+            task_respo = requests.get(f'{api_url}/todos').json()
+            name_emplo = user_respo.get('name')
+            to_do = list(filter(lambda i: i.get('userId') == id, task_respo))
+            comp_task = list(filter(lambda i: i.get('completed'), to_do))            
             print(
-                "Employee {} is done with to_do({}/{}):".format(
-                    name_emplo, len(comp_task), len(to_do)
+                'Employee {} is done with to_do({}/{}):'.format(
+                    name_emplo,
+                    len(comp_task),
+                    len(to_do)
                 )
             )
             if comp_task:
                 for task in comp_task:
-                    print("\t {}".format(task.get("title")))
+                    print('\t {}'.format(task.get('title')))
+

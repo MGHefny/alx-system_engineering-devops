585667dc0813f27463cf134bf79a6f8d0fc572c0
MGHefny <mgenedhefny@gmail.com>
2024-05-21 03:04:55 +0300

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,21 +3,19 @@
 import csv
 import requests
 import sys
-
 api_url = "https://jsonplaceholder.typicode.com"
-if __name__ == "__main__":
+if __name__ == '__main__':
     user_id = sys.argv[1]
-    user_respo = requests.get(f"{api_url}/users/{id}").json()
-    req = requests.get(user_respo)
-    u_name = req.json().get("username")
-    task_respo = requests.get(f"{api_url}/todos").json()
+    user_respo = f'{api_url}/users/{user_id}'
+    req = requests.get(url_user)
+    u_name = req.json().get('username')
+    task_respo = f'{url_user}/todos'
     req = requests.get(task_respo)
     tks = req.json()
 
-    with open("{}.csv".format(user_id), "w") as csvfile:
+    with open('{}.csv'.format(user_id), 'w') as csvfile:
         for task_respo in tks:
-            comp = task_respo.get("completed")
-            title_task = task_respo.get("title")
-            csvfile.write(
-                '"{}","{}","{}","{}"\n'.format(
-                           user_id, u_name, comp, title_task))
+            comp = task_respo.get('completed')
+            title_task = task_respo.get('title')
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user_id, u_name, comp, title_task))

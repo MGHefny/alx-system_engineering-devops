8441fd0fa585a518ef6ca900a9deb31a5a34043e
MGHefny <mgenedhefny@gmail.com>
2024-05-21 03:12:31 +0300

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,19 +3,19 @@
 import csv
 import requests
 import sys
-api_url = "https://jsonplaceholder.typicode.com"
+
 if __name__ == '__main__':
     user_id = sys.argv[1]
-    user_respo = f'{api_url}/users/{user_id}'
-    req = requests.get(url_user)
-    u_name = req.json().get('username')
-    task = f'{url_user}/todos'
-    req = requests.get(task)
-    tasks = req.json()
+    u_uesr = 'https://jsonplaceholder.typicode.com/users/' + user_id
+    reqq = requests.get(u_uesr)
+    u_name = reqq.json().get('username')
+    tsk = u_uesr + '/todos'
+    reqq = requests.get(tsk)
+    tsks = reqq.json()
 
     with open('{}.csv'.format(user_id), 'w') as csvfile:
-        for task in tasks:
-            comp = task.get('completed')
-            title_task = task.get('title')
+        for tsk in tsks:
+            comp = tsk.get('completed')
+            t_tsk = tsk.get('title')
             csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user_id, u_name, comp, title_task))
+                user_id, u_name, comp, t_tsk))

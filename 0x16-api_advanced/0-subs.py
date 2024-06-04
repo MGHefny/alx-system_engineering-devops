aed699ec0d9c4f7901f22f3f9680529788f05d38
MGHefny <mgenedhefny@gmail.com>
2024-06-05 02:33:56 +0300

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """test"""
-    
-    api_inf = requests.get("https://www.reddit.com/r/{}/about.json"
-            .format(subreddit),
-            headers={"User-agent": "agent_browser"},
-            allow_redirects=False)
+    api_inf = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-agent": "agent_browser"},
+        allow_redirects=False,
+    )
     if api_inf.status_code != 200:
         return 0
+
 
 return api_inf.json().get("data").get("subscribers")

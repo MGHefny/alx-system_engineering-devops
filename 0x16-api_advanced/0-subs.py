ebd404196722e2cb4110f87f3904289f016a7772
MGHefny <mgenedhefny@gmail.com>
2024-06-05 02:49:08 +0300

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
     api_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    users = {'User-agent': 'Google Chrome V 125.0.6422.142'}
+    users = {'User-Agent': 'Google Chrome Version 125.0.6422.142'}
     resp = get(api_url, headers=users)
     res = resp.json()
     try:

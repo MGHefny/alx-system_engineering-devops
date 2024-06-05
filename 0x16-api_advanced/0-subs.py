3e8039010606b145ed8eef531727f9ac4f528f18
MGHefny <mgenedhefny@gmail.com>
2024-06-05 03:04:03 +0300

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """api"""
-
-import requests
+import req
 
 
 def number_of_subscribers(subreddit):
-    """test subscriber"""
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-    api_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    users = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-                    Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'}
-    resp = get(api_url, headers=users)
-    res = resp.json()
-    try:
-        return res.get('data').get('subscribers')
-    except Exception:
-        return 0
+    """test"""
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    head = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    res = req.get(
+        url,
+        headers=head,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0

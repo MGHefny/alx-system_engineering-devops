1a7c196445f2090dcd7e0548a3203ecad5a934e0
MGHefny <mgenedhefny@gmail.com>
2024-06-13 21:31:11 +0300

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,11 @@
-#fix error open file
+#fix error opened file
 
 exec {'max-file-open':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  command => 'sed -i "s/15/5000/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
+}
+
+exec { 're-nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }

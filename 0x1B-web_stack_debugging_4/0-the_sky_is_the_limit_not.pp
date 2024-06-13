fb8b28d5177ab34cb6d31049f01be705a7f5bfc1
MGHefny <mgenedhefny@gmail.com>
2024-06-13 21:24:59 +0300

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 exec {'max-file-open':
   command => 'sed -i "s/15/5000/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
 
 exec { 're-nginx':
